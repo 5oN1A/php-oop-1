0b60ad84dd3a11d304c2741a2fbbceea0f3d2bf7
5oN1A <82112240+5oN1A@users.noreply.github.com>
2021-12-02 17:46:14 +0100

    cards printed

--- classes/Movie.php
+++ classes/Movie.php
@@ -1,53 +1,74 @@
-<?php 
+<?php
 
 
-    class Movie {
-        public $title = "";
-        public $categories = [];
-        public $score = "";
-        public $released = false;
+class Movie
+{
+    public $title = "";
+    public $img = "";
+    public $categories = [];
+    public $score = "";
+    public $released = false;
+    
 
 
-       function __construct($movie_title)
-        {
-            $this->title = $movie_title;
-        }
-
-        /****************************************************************************** */
-        //funzioni CATEGORIES
-
-        public function setCategories($categoriesString) {
-            $this->categories = explode(",", $categoriesString);
-        }
-
-        public function getCategories() {
-            return $this->categories ;
-        }
-
-         /****************************************************************************** */
-        //funzioni SCORE
-
-        public function setScore($scorePercentage) {
-            $this->score = ($scorePercentage * 5)/ 100;
-        }
-
-        public function getScore() {
-            return $this->score;
-        }
-
-         /****************************************************************************** */
-        //funzioni RELEASED 
-
-        public function setRelease($releaseDate) {
-            if (isset($releaseDate)) {
-                $this->released = true;
-            }
-        }
-            
-            public function getRelease() {
-                return $this->released;
-            }
-        
-
-
+    function __construct($movie_title)
+    {
+        $this->title = $movie_title;
     }
+    /****************************************************************************** */
+    //funzioni IMG
+    public function setImg($imgPath)
+    {
+        $root = "https://www.themoviedb.org/t/p/w600_and_h900_bestv2/";
+        $this->img = $root . $imgPath;
+    }
+
+    public function getImg()
+    {
+        return $this->img;
+    }
+
+    //https://www.themoviedb.org/t/p/w600_and_h900_bestv2/aKfxlsL0o2M4aBmX8NMFXlXFKCg.jpg
+    //https://www.themoviedb.org/t/p/w600_and_h900_bestv2/sSdRI34rbqXj21SruV52AUoXRkE.jpg
+
+    /****************************************************************************** */
+    //funzioni CATEGORIES
+
+    public function setCategories($categoriesString)
+    {
+        $this->categories = explode(",", $categoriesString);
+    }
+
+    public function getCategories()
+    {
+        return $this->categories;
+    }
+
+    /****************************************************************************** */
+    //funzioni SCORE
+
+    public function setScore($scorePercentage)
+    {
+        $this->score = ($scorePercentage * 5) / 100;
+    }
+
+    public function getScore()
+    {
+        return $this->score;
+    }
+
+    /****************************************************************************** */
+    //funzioni RELEASED 
+
+    public function setRelease($releaseDate)
+    {
+        if (isset($releaseDate)) {
+            $this->released = true;
+        }
+    }
+
+    public function getRelease()
+    {
+        return $this->released;
+    }
+}
--- index.php
+++ index.php
@@ -5,10 +5,13 @@
    => all’interno della classe è definito almeno un metodo
 - vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
 
-<?php 
+<?php
 require_once "./classes/Movie.php";
 
 $movie1 = new Movie("Venom - La furia di Carnage");
+
+$movie1->setImg("sSdRI34rbqXj21SruV52AUoXRkE.jpg");
+$movie1->getImg();
 
 $movie1->setCategories("Fantascienza,Azione,Avventura");
 $movie1->getCategories();
@@ -20,8 +23,12 @@ $movie1->setRelease(null);
 $movie1->getRelease();
 
 
-var_dump($movie1);
+//var_dump($movie1);
+
 $movie2 = new Movie("Clifford - Il grande cane rosso");
+
+$movie2->setImg("aKfxlsL0o2M4aBmX8NMFXlXFKCg.jpg");
+$movie2->getImg();
 
 $movie2->setCategories("Animazione, Commedia, Famiglia");
 $movie2->getCategories();
@@ -32,7 +39,87 @@ $movie2->getScore();
 $movie2->setRelease("10/11/2021");
 $movie2->getRelease();
 
-var_dump($movie2);
+//var_dump($movie2);
 
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latest Movies</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+    <header>
+    <h1 class="my-5 text-center">LATEST MOVIES</h1>
+    </header>
+
+    <div class="container ">
+    <div class="d-flex justify-content-center">
+
+            <div class="card mx-5" style="width: 18rem;">
+                <img src="<?php echo $movie1->img ?>" class="card-img-top" alt=<?php echo $movie1->title ?>>
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $movie1->title ?></h5>
+                    <p class="card-text"><strong>Score:</strong> <?php echo $movie1->score ?>/5</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <?php
+
+                    foreach ($movie1->categories as $category) {
+                        echo "<li class='list-group-item'>$category</li>";
+                    }
+
+                    ?>
+
+                </ul>
+                <div class="card-body">
+                    <p><?php if ($movie1->released) {
+                            echo "already released";
+                        } else {
+                            echo "to be released";
+                        }
+                        ?></p>
+                </div>
+            </div>
+
+            <div class="card mx-5" style="width: 18rem;">
+                <img src="<?php echo $movie2->img ?>" class="card-img-top" alt=<?php echo $movie2->title ?>>
+                <div class="card-body">
+                    <h5 class="card-title"><?php echo $movie2->title ?></h5>
+                    <p class="card-text"><strong>Score:</strong> <?php echo $movie1->score ?>/5</p>
+                </div>
+                <ul class="list-group list-group-flush">
+                    <?php
+
+                    foreach ($movie2->categories as $category) {
+                        echo "<li class='list-group-item'>$category</li>";
+                    }
+
+                    ?>
+
+                </ul>
+                <div class="card-body">
+                    <p><?php if ($movie2->released) {
+                            echo "Already released";
+                        } else {
+                            echo "To be released";
+                        }
+                        ?></p>
+                </div>
+            </div>
+    </div>
+    </div>
+
+
+</body>
+
+</html>
